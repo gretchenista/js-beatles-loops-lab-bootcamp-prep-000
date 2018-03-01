be44b2c9eddcf0ad91eac9d8f5afb95c8b352791
Cutina Woof <gretchenista@gmail.com>
2018-03-01 00:13:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function theBeatlesPlay (musicians, instruments) {
+  var array = [ ];
+  for (var i = 0; i < musicians.length; i++) {
+  array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+
+function johnLennonFacts (facts) {
+  var array = [ ];
+  var i = 0;
+  while (i < facts.length) {
+  array.push(facts[i] + "!!!");
+  }
+  i++;
+  return array;
+}
+
+function iLoveTheBeatles () {
+  var array = [ ];
+  var i = 0;
+  i++;
+  do {
+    return ("I love the Beatles!");
+  } while (i < 15) {
+  }
+  return ("I love the Beatles!")
+}
